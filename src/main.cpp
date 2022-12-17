d1c81781344498d3a212f2035257132cf655f623
Bartosz <kwiatkowsky.kontakt@gmail.com>
2022-12-17 21:52:45 +0100

    Working

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,72 @@
 #include <Arduino.h>
 
-#define NUM_LINES_MASTER  8
-#define NUM_LINES_SLAVE  7
+// 74HC595
+int clockPin = 9;
+int latchPin = 10;
+int dataPin = 11;
+byte leds = 0;
+int currentLED = 0;
 
-// Define Connections to 74H165
+// 74HC165
 int load = 7;
 int clockEnablePin = 4;
 int dataIn = 5;
 int clockIn = 6;
 
-// Define Connections to 74HC595
-const int latchPin = 10;
-const int clockPin = 11;
-const int dataPin = 12;
-int iterator = 7;
-byte master = 0;
-
 void setup()
 {
+  // Setup Serial Monitor
   Serial.begin(9600);
+
+  // Setup 74HC595 connections
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
 
   // Setup 74HC165 connections
   pinMode(load, OUTPUT);
   pinMode(clockEnablePin, OUTPUT);
   pinMode(clockIn, OUTPUT);
   pinMode(dataIn, INPUT);
-
-  // Setup pins as Outputs
-  pinMode(latchPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
-  
-  // set all zero
-
 }
 
 void loop()
 {
-  iterator = 0;
-  if (iterator == 7) {
-    iterator = 0;
-  } else {
-    iterator++;
-  }
 
-  bitSet(master, iterator);
+  for(uint8_t i = 0; i < 8; ++i) {
+    
+    bitSet(leds, i);
 
-  // Write pulse to load pin
-  digitalWrite(load, LOW);
-  delayMicroseconds(5);
-  digitalWrite(load, HIGH);
-  delayMicroseconds(5);
-
-  for(uint8_t i = 0; i < NUM_LINES_MASTER; ++i) {
+    // set current master to active
     digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, i);
+    shiftOut(dataPin, clockPin, LSBFIRST, leds);
     digitalWrite(latchPin, HIGH);
-  }
 
-  for(uint8_t i = 0; i < NUM_LINES_MASTER; ++i) {
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, i);
-    digitalWrite(latchPin, LOW);
+    // Write pulse to load pin
+    digitalWrite(load, LOW);
+    delayMicroseconds(5);
+    digitalWrite(load, HIGH);
+    delayMicroseconds(5);
 
     // Get data from 74HC165
     digitalWrite(clockIn, HIGH);
     digitalWrite(clockEnablePin, LOW);
     byte incoming = shiftIn(dataIn, clockIn, LSBFIRST);
     digitalWrite(clockEnablePin, HIGH);
+
+    bitClear(leds, i);
+
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, leds);
+    digitalWrite(latchPin, HIGH);
+
     // Print to serial monitor
     Serial.print("Master ");
     Serial.print(i);
-    Serial.print(". Pin States:\r\n");
+    Serial.print(". ");
     Serial.println(incoming, BIN);
-  }
 
-  delay(100);
+  
+  }
+  delay(250);
 }
